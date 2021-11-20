fc10da28702a1699dfb35dd0f930cf55c7cea9bc
saloni137 <salonisaraiya@yahoo.in>
2021-11-20 11:14:23 +0530

    first commit

---
+++ CrudApplication/src/config/app.ts
@@ -0,0 +1,35 @@
+import  express from "express";
+import * as bodyParser from "body-parser";
+import * as mongoose from 'mongoose';
+import { TestRoutes } from "../routes/user_routes";
+import { CommonRoutes } from "../routes/common_routes";
+
+class App {
+
+   public app: express.Application;
+   public mongoUrl: string = 'mongodb://localhost/sample-database';
+
+   private test_routes: TestRoutes = new TestRoutes();
+   private common_routes: CommonRoutes = new CommonRoutes();
+
+   constructor() {
+        this.app = express();
+        this.config();
+        this.mongoSetup();
+        this.test_routes.route(this.app);
+        this.common_routes.route(this.app);
+   }
+
+   private config(): void {
+      // support application/json type post data
+      this.app.use(bodyParser.json());
+      //support application/x-www-form-urlencoded post data
+      this.app.use(bodyParser.urlencoded({ extended: false }));
+   }
+
+   private mongoSetup(): void {
+      mongoose.connect(this.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+   }
+
+}
+export default new App().app;
---
+++ CrudApplication/src/server.ts
@@ -0,0 +1,4 @@
+import app from "./config/app";
+app.listen(5000,()=>{
+    console.log('server running');
+});
